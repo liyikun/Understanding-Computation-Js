39c9355580335e2b918b42cb2225276561d55c2d
liyikun <liyikunl@live.com>
2018-04-16 11:56:37 +0800

    update Finite_Automaton

--- Finite_Automaton/3_1_4_example.js
+++ Finite_Automaton/3_1_4_example.js
@@ -35,8 +35,40 @@ const rulebook = new DFARulebook([
     new FARule(1, 'a', 2),new FARule(1, 'b', 1),new FARule(2, 'a', 2),new FARule(2, 'b', 3),new FARule(3, 'a', 3),new FARule(3, 'b', 3)
 ])
 
-console.log(rulebook.next_state(1, 'a'))
+// console.log(rulebook.next_state(1, 'a'))
 
-console.log(rulebook.next_state(1, 'b'))
+// console.log(rulebook.next_state(1, 'b'))
 
-console.log(rulebook.next_state(2, 'b'))
+// console.log(rulebook.next_state(2, 'b'))
+
+
+class DFA {
+    constructor(current_state,accept_states,rulebook) {
+        this.current_state = current_state;
+        this.accept_states = accept_states;
+        this.rulebook = rulebook;
+    }
+    accepting() {
+        return this.accept_states.includes(this.current_state)
+    }
+    read_character(character) {
+        this.current_state = rulebook.next_state(this.current_state, character)
+    }
+}
+
+// console.log(new DFA(1, [1, 3], rulebook).accepting())
+
+const dfa = new DFA(1, [3], rulebook)
+
+dfa.read_character('b')
+console.log(dfa.accepting())
+const rundo = [1,2,3];
+rundo.forEach(e => {
+    dfa.read_character('a')
+});
+console.log(dfa.accepting())
+dfa.read_character('b')
+
+console.log(dfa.accepting())
+
+
