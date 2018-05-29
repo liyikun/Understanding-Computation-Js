142332b269c40b867ec2ac1d3e7ff9d4c2b6b05a
liyikun <liyikunl@live.com>
2018-05-29 23:33:03 +0800

    add free move

--- Finite_Automaton/NFA.js
+++ Finite_Automaton/NFA.js
@@ -1,4 +1,19 @@
 const _ = require("lodash")
+
+Set.prototype.subset = function (otherSet) {  
+    if (this.size > otherSet.size) {  
+        return false;  
+    } else {  
+        var values = this.values();  
+        for (var i = 0; i < values.length; i++) {  
+            if (!otherSet.has(values[i])) {  
+                return false;  
+            }  
+        }  
+        return true;  
+    }  
+}
+
 class FARule {
     constructor(state, character, next_state) {
         this.state = state;
@@ -33,6 +48,15 @@ class NFARuleBook {
             return rule.appliesto(state, character)
         })
     }
+    follow_free_moves(states) {
+        let more_states = this.next_states(states, null)
+        let states_set = new Set(states)
+        if(more_states.subset(states_set)) {
+            return states_set
+        } else {
+            return this.follow_free_moves([...states_set,...more_states])
+        }
+    }
 }
 
 const rulebook = new NFARuleBook([
@@ -51,6 +75,7 @@ console.log(rulebook.next_states([1, 2], 'a'))
 console.log(rulebook.next_states([1, 3], 'b'))
 
 
+
 class NFA {
     constructor(current_state, accept_state, rulebook) {
         this.current_state = current_state;
@@ -58,10 +83,10 @@ class NFA {
         this.rulebook = rulebook;
     }
     accepting() {
-        return this.current_state.indexOf(this.accept_state)
+        return this.current_state.filter(x => this.accept_state.includes(x)).length > 0
     }
     read_character(character) {
-        return this.current_state = this.rulebook.next_states(this.current_states, character)
+        return this.current_state = Array.from(this.rulebook.next_states(this.current_state, character))
     }
     read_string(string) {
         return string.forEach(character => {
@@ -70,4 +95,24 @@ class NFA {
     }
 }
 
+// const nfa1 = new NFA([1], [4], rulebook)
+// console.log(nfa1.accepting())
+// console.log(nfa1.read_character('b'))
+// console.log(nfa1.accepting())
+// console.log(nfa1.read_character('a'))
+// console.log(nfa1.accepting())
+// console.log(nfa1.read_character('b'))
+// console.log(nfa1.accepting())
 
+
+
+const rulebook2 = new NFARuleBook([
+    new FARule(1, null, 2),new FARule(1, null, 4),
+    new FARule(2, 'a', 3),new FARule(3, 'a', 2),
+    new FARule(4, 'a', 5), new FARule(5, 'a', 6),
+    new FARule(6, 'a', 4)
+])
+
+console.log(...rulebook2.next_states([1], null).values())
+
+console.log(...rulebook2.follow_free_moves([1]).values())
